08646d5e76e73e41712ca5f075f1b8409f194553
Shania Lopez <slopez@fandm.edu>
2023-05-04 18:54:06 -0400

    now the heart button will append what has been liked, but not how many times

--- js/challenge.js
+++ js/challenge.js
@@ -1,47 +1,67 @@
-document.addEventListener('DOMContentLoaded', doTimer)
-let count = 0;
-let counterDisplay = document.getElementById("counter")
-let counterIncrease = document.getElementById("plus")
-let counterDecrease =  document.getElementById("minus")
-let pause = document.getElementById("pause")
-let cnt = initializeCounter
 
+// grab all of the buttons and assign them to variables
+const counterDisplay = document.getElementById("counter");
+const counterIncrease = document.getElementById("plus");
+const counterDecrease =  document.getElementById("minus");
+const pause = document.getElementById("pause");
+const heart = document.getElementById("heart");
+const buttons = [counterIncrease, counterDecrease, heart]
+
+
+let count = 0; 
+let paused = false;
+let timer = setInterval(initializeCounter, 1000)
+// initialize counter
 function initializeCounter(){
+    if (paused === false){
     count++;
-    counterDisplay.innerHTML = count
-    
+    counterDisplay.innerText = `${count}`;
 }
-
-let timer; 
-
-
-
+}
+// plus button to increase the counter by one
 counterIncrease.addEventListener('click', () => {
         count += 1;
-        counterDisplay.innerHTML = count;
+        counterDisplay.innerText = `${count}`;
 
     })
+
+// minus button to decrease the counter by one
 
 counterDecrease.addEventListener('click', () => {
         count -= 1;
-        counterDisplay.innerHTML = count;
-
+        counterDisplay.innerText = `${count}`;
     })
+// pause button, switch the text if the condition matches 
+// disable the buttons except for pause
 
-function doTimer() {
-    pause.addEventListener('click', () => {
+pause.addEventListener('click', () => {
+    if (pause.innerText === 'pause'){
+        pause.innerText = 'resume';
+        buttons.forEach(function(element){
+            element.disabled = true;
+        }); 
+        paused = !paused;
+    } else {
+        pause.innerText = 'pause';
+        buttons.forEach(function(element) {
+            element.disabled = false;
+        });
+        paused = !paused;
+    }
+});
 
-    if (!timer){
-        timer = setInterval(initializeCounter, 1000)
-        pause.innerHTML = 'pause';
-     } else {
-        clearInterval(timer);
-        timer = false
-        pause.innerHTML = 'resume'
-     }
-})}
+heart.addEventListener('click', () => {
+    let likes = document.querySelector('.likes');
+    let currentCount = counterDisplay.innerText;
+    let li = document.createElement("li");
+
+    
+    let liText = document.createTextNode(`${currentCount} has been liked `);
+    likes.appendChild(li)
+    li.append(liText)
 
 
+})
 
 // function countingUp(){
 //     cntr.addEventListener('DOMContentLoaded', () => {
