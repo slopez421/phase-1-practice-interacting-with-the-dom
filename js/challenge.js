92c7786f7fc0f2b2d47d8000bcdea306e386b2f4
Shania Lopez <slopez@fandm.edu>
2023-05-04 19:35:45 -0400

    complete lab sort of

--- js/challenge.js
+++ js/challenge.js
@@ -5,12 +5,14 @@ const counterIncrease = document.getElementById("plus");
 const counterDecrease =  document.getElementById("minus");
 const pause = document.getElementById("pause");
 const heart = document.getElementById("heart");
-const buttons = [counterIncrease, counterDecrease, heart]
+const submit = document.getElementById("submit")
+const buttons = [counterIncrease, counterDecrease, heart, submit]
 
 
 let count = 0; 
 let paused = false;
 let timer = setInterval(initializeCounter, 1000)
+
 // initialize counter
 function initializeCounter(){
     if (paused === false){
@@ -53,16 +55,46 @@ pause.addEventListener('click', () => {
 heart.addEventListener('click', () => {
     let likes = document.querySelector('.likes');
     let currentCount = counterDisplay.innerText;
-    let li = document.createElement("li");
+    let a = document.getElementById(currentCount)
+// assign the currentCount to another variable, otherwise we'll
+//end up changing the innerText directly  
 
-    
-    let liText = document.createTextNode(`${currentCount} has been liked `);
-    likes.appendChild(li)
-    li.append(liText)
+    if(a) {
+        let text = a.innerText;
+        let textArray = text.split(" ");
+        // Number() converts a string or value directly to a number type
+        //here, we're taking the inner text of currentCount and converting it into a number type instead of a string
+        let number = Number(textArray.slice(-2,-1));
+        //so we can add it + 1 every time it is liked
+        a.innerHTML = `${currentCount} has been liked ${number + 1} times`;
+    } else {
+        let a = document.createElement('li');
+        a.setAttribute("id", currentCount)
+        likes.appendChild(a);
+        a.innerHTML = `${currentCount} has been liked 1 time`;
+    }
 
 
 })
 
+submit.addEventListener('click', (e) => {
+    //prevents from the automatic refresh
+    e.preventDefault();
+    // grab the comment list section
+    let comment = document.getElementById("list");
+    //create a p tag for each comment that will get put throught the comment-input
+    let p = document.createElement("p");
+    // append each p to the list / comment section
+    comment.appendChild(p);
+    // should re-name comments, come back late
+    // the input bar is now assigned to comments
+    let comments = document.getElementById("comment-input");
+    // but to get the actual text of the comment bar instead of the <input>, we need to use comments.value
+    // and assign it to the p element that gets appended to the DOM
+    p.innerText = `${comments.value}`;
+})
+
+// failed first attempt
 // function countingUp(){
 //     cntr.addEventListener('DOMContentLoaded', () => {
 //         count++;
